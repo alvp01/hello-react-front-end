50abfd9cbf121b7d31804dc3ed8185b53a64eb83
Abel Lavieri <abel.lavieri@gmail.com>
2023-12-16 15:38:29 -0400

    Add the app basic functionality to fetch a greeting and display it

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,29 @@
+import { useSelector } from 'react-redux';
+
+function Greeting() {
+  const {
+    greeting, isLoading, error,
+  } = useSelector((state) => state.greeting);
+
+  let loadMessage = null;
+
+  if (isLoading) {
+    loadMessage = 'Loading message...';
+  }
+
+  if (error) {
+    loadMessage = 'Error loading data';
+  }
+
+  return (
+    <section>
+      {
+      (isLoading || error)
+        ? (<p className="status"><i>{loadMessage}</i></p>)
+        : (<p className="greeting-message"><b>{greeting?.message}</b></p>)
+    }
+    </section>
+  );
+}
+
+export default Greeting;
